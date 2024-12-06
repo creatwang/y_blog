5a81c63b926561a447b1ff560e624e6f7039e510
wangyanan <wangyanan@idd.cool>
2024-12-06 16:59:57 +0800

    init: 初始化文档应用

---
+++ app/app.config.ts
@@ -0,0 +1,63 @@
+export default defineAppConfig({
+  ui: {
+    primary: '#F6821F',
+    gray: 'slate',
+    footer: {
+      bottom: {
+        left: 'text-sm text-gray-500 dark:text-gray-400',
+        wrapper: 'border-t border-gray-200 dark:border-gray-800'
+      }
+    }
+  },
+  seo: {
+    siteName: 'Levill web site'
+  },
+  header: {
+    logo: {
+      alt: '随笔',
+      light: '/logo.png',
+      dark: '/logo.png'
+    },
+    search: true,
+    colorMode: true,
+    links: [{
+      'icon': 'i-simple-icons-github',
+      'to': 'https://github.com/creatwang',
+      'target': '_blank',
+      'aria-label': 'GitHub'
+    }]
+  },
+  footer: {
+    credits: 'Copyright © 2024',
+    colorMode: false,
+    links: [{
+      'icon': 'i-simple-icons-discord',
+      'to': 'https://discord.com/invite/ps2h6QT',
+      'target': '_blank',
+      'aria-label': 'Nuxt UI on Discord'
+    }]
+  },
+  toc: {
+    title: 'Table of Contents',
+    bottom: {
+      /* title: 'Community',
+      edit: 'https://github.com/nuxt-ui-pro/docs/edit/main/content',
+      links: [{
+        icon: 'i-heroicons-star',
+        label: 'Star on GitHub',
+        to: 'https://github.com/nuxt/ui',
+        target: '_blank'
+      }, {
+        icon: 'i-heroicons-book-open',
+        label: 'Nuxt UI Pro docs',
+        to: 'https://ui.nuxt.com/pro/guide',
+        target: '_blank'
+      }, {
+        icon: 'i-simple-icons-nuxtdotjs',
+        label: 'Purchase a license',
+        to: 'https://ui.nuxt.com/pro/purchase',
+        target: '_blank'
+      }] */
+    }
+  }
+})
